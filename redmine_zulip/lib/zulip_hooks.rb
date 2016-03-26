58ce1a84de7fc8e0daca2045f631a47f9ba9607a
Akihiro Watanabe <a.watanabe.4891@gmail.com>
2016-03-26 23:09:55 +0000

    メッセージテンプレート対応

--- redmine_zulip/lib/zulip_hooks.rb
+++ redmine_zulip/lib/zulip_hooks.rb
@@ -3,7 +3,8 @@
 require 'json'
 
 class NotificationHook < Redmine::Hook::Listener
-
+    include ApplicationHelper
+    include CustomFieldsHelper
     # We generate Zulips for creating and updating issues.
 
     def controller_issues_new_after_save(context = {})
@@ -15,17 +16,9 @@ class NotificationHook < Redmine::Hook::Listener
              return true
         end
 
-        content = %Q{%s opened [issue %d: %s](%s) in %s
-
-~~~ quote
-%s
-~~~
-
-**Priority**: %s
-**Status**: %s
-**Assigned to**: %s} % [User.current.name, issue.id, issue.subject, url(issue),
-                        project.name, issue.description, issue.priority.to_s,
-                        issue.status.to_s, issue.assigned_to.to_s]
+        msg = project.zulip_message_new
+        msg = Setting.plugin_redmine_zulip[:zulip_message_new] if msg == ""
+        content = zulip_message(context, msg)
 
         send_zulip_message(content, project)
     end
@@ -39,12 +32,9 @@ class NotificationHook < Redmine::Hook::Listener
              return true
         end
 
-        content = %Q{%s updated [issue %d: %s](%s) in %s
-
-~~~ quote
-%s
-~~~} % [User.current.name, issue.id, issue.subject, url(issue),
-        project.name, issue.notes]
+        msg = project.zulip_message_edit
+        msg = Setting.plugin_redmine_zulip[:zulip_message_edit] if msg == ""
+        content = zulip_message(context, msg)
 
         send_zulip_message(content, project)
     end
@@ -72,6 +62,39 @@ class NotificationHook < Redmine::Hook::Listener
         return false
     end
 
+    def zulip_message(context, msg_template)
+        issue = context[:issue]
+        project = issue.project
+
+        std_fields = %(id tracker status priority author assigned_to subject description notes start_date due_date done_ratio spent_hours)
+        msg = msg_template
+        msg.gsub!(/%\{([^\}]+)\}/) do |v|
+            word = $1
+            case word
+            when 'username' then User.current.name
+            when 'url' then url(issue)
+            when 'projectname' then project.name
+            when /^(.+?)(:([^:]+))?$/ then
+                str = $1
+                cond = $3
+                if cond then
+                    tracker = issue.tracker.to_s
+                    next "" if tracker != cond
+                end
+                if std_fields.include?(str) then
+                    issue.__send__(str).to_s
+                elsif str =~ /^'(.*)'$/ then
+                    $1
+                else
+                    cv = issue.custom_field_values.detect {|c| c.custom_field.name == str}
+                    cv ? show_value(cv, false) : word
+                end
+            else word
+            end
+        end
+        return msg
+    end
+
     def zulip_email(project)
         if !project.zulip_email.empty?
             return project.zulip_email
@@ -93,23 +116,23 @@ class NotificationHook < Redmine::Hook::Listener
         return Setting.plugin_redmine_zulip[:zulip_stream]
     end
 
-   def zulip_server()
-       return Setting.plugin_redmine_zulip[:zulip_server]
-   end
+    def zulip_server()
+        return Setting.plugin_redmine_zulip[:zulip_server]
+    end
    
-   def zulip_port()
-      if Setting.plugin_redmine_zulip[:zulip_port]
-        return Setting.plugin_redmine_zulip[:zulip_port]
-      end
-      return 443
-   end
+    def zulip_port()
+        if Setting.plugin_redmine_zulip[:zulip_port]
+            return Setting.plugin_redmine_zulip[:zulip_port]
+        end
+        return 443
+    end
 
-   def zulip_api_basename()
-     if Setting.plugin_redmine_zulip[:zulip_server]["api.zulip.com"]
-       return ""
-     end
-     return "/api"
-   end
+    def zulip_api_basename()
+        if Setting.plugin_redmine_zulip[:zulip_server]["api.zulip.com"]
+            return ""
+        end
+        return "/api"
+    end
 
     def url(issue)
         return "#{Setting[:protocol]}://#{Setting[:host_name]}/issues/#{issue.id}"
@@ -126,6 +149,7 @@ class NotificationHook < Redmine::Hook::Listener
 
         http = Net::HTTP.new(zulip_server(), zulip_port())
         http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         req = Net::HTTP::Post.new(zulip_api_basename() + "/v1/messages")
         req.basic_auth zulip_email(project), zulip_api_key(project)
@@ -134,7 +158,7 @@ class NotificationHook < Redmine::Hook::Listener
 
         begin
             http.request(req)
-        rescue Net::HTTPBadResponse => e
+        rescue => e
             Rails.logger.error "Error while POSTing to Zulip: #{e}"
         end
     end
